3c2a0fa042b87e3a84fad18aa7160fc7b07518c9
MiguelAngelBustos <44479306+MiguelAngelBustos@users.noreply.github.com>
2019-05-16 20:16:37 +0200

    version 1.1

--- BotonMania/src/juego.java
+++ BotonMania/src/juego.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 
 public class juego {
-	public static int mov = 0;
-	public static double punt = 0;
-	public static int fil;
-	public static int col;
-	public static int[][] tab = new int[7][7];
-	public static int niv;
+	protected static int mov = 0;
+	protected static double punt = 0;
+	protected static int fil;
+	protected static int col;
+	protected static int[][] tab = new int[7][7];
+	//protected static int niv;
+	protected static Scanner t = new Scanner(System.in);
 
 	juego() {
 
@@ -15,8 +17,71 @@ public class juego {
 
 	}
 
-	public static int niveles() {
+	public static int niveles(int niv) {
+		int opc = 4;
+		int aux = 2;
+		String nlvl;
+		do {
+		try {
+			System.out.println("¿Desea cambiar de nivel?");
+			System.out.println("1.-SI");
+			System.out.println("2.-NO");
+			aux = t.nextInt();
+		} catch (Exception e) {
+			System.out.println("ERROR AL ELEGIR EL NIVEL");
+		}
+		}while(aux!=1 || aux!=2);
+		
+		if (aux == 1) {
+			do {
+				try {
+					System.out.println("¿Que nivel desea?");
+					System.out.println("1.-NOVATO");
+					System.out.println("2.-SEMI-NOVATO");
+					System.out.println("3.-INTERMEDIO");
+					System.out.println("4.-INTERMEDIO-ALTO");
+					System.out.println("5.-ALTO");
+					System.out.println("6.-MUY ALTO");
+					System.out.println("7.-EXPERTO");
+					System.out.println("8.-SEMI-DIOS");
+					System.out.println("9.-DIOS");
+					opc = t.nextInt();
+				} catch (Exception e) {
+					System.out.println("ERROR AL ELEGIR EL NIVEL");
+				}
+				}while(opc<1 || opc>9);
+			switch(opc) {
+			case 1:
+				nlvl = "Novato";
+				break;
+			case 2:
+				nlvl = "Semi-Novato";
+				break;
+			case 3:
+				nlvl = "Intermedio";
+				break;
+			case 4:
+				nlvl = "Intermedio-alto";
+				break;
+			case 5:
+				nlvl = "Alto";
+				break;
+			case 6:
+				nlvl = "Muy Alto";
+				break;
+			case 7:
+				nlvl = "Experto";
+				break;
+			case 8:
+				nlvl = "Semi-Dios";
+				break;
+			case 9:
+				nlvl = "Dios";
+				break;
+			}
+		}
 
+		niv = opc;
 		return niv;
 	}
 
@@ -28,7 +93,9 @@ public class juego {
 
 	}
 
-	public static void mensaje() {
-
+	public static void mensaje(int niv) {
+		int punt;
+		int golp;
+		int aux;
 	}
 }
