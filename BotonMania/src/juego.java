c5038c77f3967286bca9951db31a155a4b16dd9b
MiguelAngelBustos <44479306+MiguelAngelBustos@users.noreply.github.com>
2019-05-16 19:45:19 +0200

    version 1.0

---
+++ BotonMania/src/juego.java
@@ -0,0 +1,34 @@
+
+public class juego {
+	public static int mov = 0;
+	public static double punt = 0;
+	public static int fil;
+	public static int col;
+	public static int[][] tab = new int[7][7];
+	public static int niv;
+
+	juego() {
+
+	}
+
+	public static void tablero() {
+
+	}
+
+	public static int niveles() {
+
+		return niv;
+	}
+
+	public static void juegos() {
+
+	}
+
+	public static void puntuacion() {
+
+	}
+
+	public static void mensaje() {
+
+	}
+}
