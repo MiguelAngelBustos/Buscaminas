f9b7095892730e34c071a4aea56110e74e4d4166
anton <anton@192.168.1.112>
2019-05-19 13:01:54 +0200

    prueba anto

--- BotonMania/src/jugada.java
+++ BotonMania/src/jugada.java
@@ -2,7 +2,7 @@
 public class jugada extends juego{
 
 	public static void main(String[] args) {
- System.out.println("Hola esto a sido cambiado por anto177 xDaa");
+ System.out.println("Hola esto a sido cambiado por anto177 ");
 	}
 
 }
