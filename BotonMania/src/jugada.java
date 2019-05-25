008ea85433f27839e3630cda9a557565e8283b59
anton <anton@192.168.1.112>
2019-05-25 11:29:01 +0200

    ver: 25/05/2019 11:28

--- BotonMania/src/jugada.java
+++ BotonMania/src/jugada.java
@@ -1,14 +1,11 @@
 
-public class jugada extends juego{
+public class jugada extends juego {
 
 	public static void main(String[] args) {
 		int[][] tab = new int[8][8];
-		
-		jugada uno = new jugada();
-		uno.tablero(niv, tab, flag);
-		
-			juegos(tab, niv);
-			puntuacion(contjugadas,golp,fil,col,niv, punt);
-			mensaje(niv, golp);
+		tablero(niv, tab, flag);
+		juegos(tab, niv, m);
+		puntuacion(contjugadas, fil, col);
+		mensaje(niv, golp);
 	}
 }
