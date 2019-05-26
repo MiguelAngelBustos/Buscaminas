7bf3a12aa89b9b1d0342226bebbb0c74cd02d46c
migue <migue@BUS-PC>
2019-05-26 11:36:02 +0200

    ver: 26/05/2019 11:35

--- ProyectoFinal/src/juego.java
+++ ProyectoFinal/src/juego.java
@@ -3,6 +3,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
 
 public class juego {
 	protected static int mov = 0;
@@ -13,7 +16,8 @@ public class juego {
 	protected static int golp;
 	protected static int m[][] = new int[8][8];
 	protected static boolean flag = false;
-	protected static int niv = 5;
+	protected static int niv=5;
+	protected static String nlvl="Alto";
 	protected static Scanner t = new Scanner(System.in);
 
 	public static int[][] tablero(int niv, int tab[][], boolean flag) {
@@ -23,18 +27,17 @@ public class juego {
 		int fil = 0;
 		int col = 0;
 
-	
 		if (flag == false) {
 
-			System.out.println("Recomenzar(0 1) - Nuevo(0 2) - Calificación(0 3) -  Cambiar Nivel(0 4) - Salir(0 -2)");
-			System.out.println("");
 			for (i = 1; i < tab.length - 1; i++) { // FILA
 				for (j = 1; j < tab[0].length - 1; j++) {
 					// COLUMNA
 					tab[i][j] = 0;
 				}
 			}
-
+			
+			
+			//GENERA LAS POSICIONES RANDOM
 			for (int z = 0; z < aux; z++) {
 
 				do {
@@ -43,53 +46,84 @@ public class juego {
 					// System.out.println(fil);
 					// System.out.println(col);
 				} while (col == 0 || fil == 0);
-
+				
+				
+				//POSICIONES DE ARRIBA ABAJO IZQUIERDA Y DERECHA
 				tab[fil][col] = tab[fil][col] + 1;
 				tab[fil + 1][col] = tab[fil + 1][col] + 1;
 				tab[fil][col + 1] = tab[fil][col + 1] + 1;
 				tab[fil][col - 1] = tab[fil][col - 1] + 1;
 				tab[fil - 1][col] = tab[fil - 1][col] + 1;
-
+				
+				//CAMBIAR LOS NUMEROS QUE SEAN IGUAL A CUATRO EN CEROS
 				for (i = 1; i < tab.length - 1; i++) {
 					for (j = 1; j < tab[0].length - 1; j++) {
 						if (tab[i][j] == 4) {
 							tab[i][j] = 0;
+
 						}
+						
+						//GUARDA TAB EN UNA MATRIZ M
+						m[i][j] = tab[i][j];
 					}
 				}
 			}
 			System.out.println();
 
-			for (i = 1; i < tab.length - 1; i++) { // FILA
-				System.out.print(" ");
-				for (j = 1; j < tab[0].length - 1; j++) {
-					// COLUMNA
-					System.out.print("|" + tab[i][j] + "|");
-				}
-				System.out.println();
-			}
-			m = tab;
 		} else {
-			tab = m;
+			
+			//SOBREESCRIBIR LA MATRIZ TAB PARA RECOMENZAR 
+			for (i = 1; i < tab.length - 1; i++) {
+				for (j = 1; j < tab[0].length - 1; j++) {
+					tab[i][j] = m[i][j];
+				}
+			}
 		}
-		return m;
+		return tab;
 	}
-	
 
-	public static int niveles(int tab[][], int niv) {
+	public static void JuegaTablero(int tab[][], double punt) {
+		
+		//PINTA LA MATRIZ TAB (EN LA QUE SE JUEGA)
+		System.out.println("Recomenzar(0 1) - Nuevo(0 2) - Calificación(0 3) -  Cambiar Nivel(0 4) - Salir(0 -2)");
+		System.out.println("");
+		System.out.println("┌───┬───┬───┬───┬───┬───┐");
+		System.out.println("│ "+tab[1][1]+" │ "+tab[1][2]+" │ "+tab[1][3]+" │ "+tab[1][4]+" │ "+tab[1][5]+" │ "+tab[1][6]+" │");
+		System.out.println("├───┼───┼───┼───┼───┼───┤");
+		System.out.println("│ "+tab[2][1]+" │ "+tab[2][2]+" │ "+tab[2][3]+" │ "+tab[2][4]+" │ "+tab[2][5]+" │ "+tab[2][6]+" │");
+		System.out.println("├───┼───┼───┼───┼───┼───┤");
+		System.out.println("│ "+tab[3][1]+" │ "+tab[3][2]+" │ "+tab[3][3]+" │ "+tab[3][4]+" │ "+tab[3][5]+" │ "+tab[3][6]+" │");
+		System.out.println("├───┼───┼───┼───┼───┼───┤");
+		System.out.println("│ "+tab[4][1]+" │ "+tab[4][2]+" │ "+tab[4][3]+" │ "+tab[4][4]+" │ "+tab[4][5]+" │ "+tab[4][6]+" │");
+		System.out.println("├───┼───┼───┼───┼───┼───┤");
+		System.out.println("│ "+tab[5][1]+" │ "+tab[5][2]+" │ "+tab[5][3]+" │ "+tab[5][4]+" │ "+tab[5][5]+" │ "+tab[5][6]+" │");
+		System.out.println("├───┼───┼───┼───┼───┼───┤");
+		System.out.println("│ "+tab[6][1]+" │ "+tab[6][2]+" │ "+tab[6][3]+" │ "+tab[6][4]+" │ "+tab[6][5]+" │ "+tab[6][6]+" │");
+		System.out.println("└───┴───┴───┴───┴───┴───┘");
+		
+		
+	/*	for (int i = 1; i < tab.length - 1; i++) { // FILA
+			System.out.print(" ");
+			for (int j = 1; j < tab[0].length - 1; j++) {
+				// COLUMNA
+				System.out.print("|" + tab[i][j] + "|");
+			}
+			System.out.println();
+		}
+		*/
+		System.out.println();
+		System.out.println("Nivel de juego: "+nlvl+" ("+niv*3+"golpes)"+" \t \t Puntuacion en nivel: "+punt);
+		System.out.print("Golpes realizados: "+golp+"\t \t \t \t Golpe (fila columna): ");
+
+	}
+
+	public static int niveles(int tab[][]) {
+
 		int opc = 5;
-		int aux = 2;
-		String nlvl;
-		/*
-		 * do { try { System.out.println("¿Desea cambiar de nivel?");
-		 * System.out.println("1.-SI"); System.out.println("2.-NO"); aux = t.nextInt();
-		 * } catch (Exception e) { System.out.println("ERROR AL ELEGIR EL NIVEL"); } }
-		 * while (aux < 1 || aux > 2);
-		 */
-
-		if (aux == 2) {
 			do {
 				try {
+					//TE DEJA ELEGIR EL NIVEL
+					System.out.println(" ");
 					System.out.println("¿Que nivel desea?");
 					System.out.println("1.-NOVATO");
 					System.out.println("2.-SEMI-NOVATO");
@@ -144,146 +178,164 @@ public class juego {
 				break;
 			}
 
-		}
-
 		niv = opc;
 		tablero(niv, tab, flag);
 		return niv;
 
 	}
 
-	public static int juegos(int[][] tab, int niv,int[][] m) {
+	public static int juegos(int[][] tab, int niv, int[][] m) {
 		Scanner t = new Scanner(System.in);
+		String aux;
+		StringTokenizer str;
+		//SALIR AL PONER EL 0 -2
 		do {
-			contjugadas++;		
+			contjugadas++;
+			flag = false;
 			do {
-				tablero(niv, tab, flag);
-				flag = false;
-				fil = t.nextInt();
-				col = t.nextInt();
-				if (fil == 0 && col == 1) {
-					// RECOMENZAR
-					flag = true;
-					tablero(niv, tab, flag);
+				try {
+					
+					JuegaTablero(tab, punt);
+					aux = t.nextLine();
+					str = new StringTokenizer(aux);
+					
+					fil=Integer.parseInt(str.nextToken());	
+					col=Integer.parseInt(str.nextToken());
+					golp++;		
+				} catch (Exception e) {
+					System.out.println("Error.");
+					System.out.println("Introduce un numero valido");
+					System.out.println();
 				}
-				if (fil == 0 && col == 2) {
-					// NUEVO
-					tablero(niv, tab, flag);
-				}
-				if (fil == 0 && col == 3) {
-					// CALIFICACION
-					puntuacion(contjugadas, golp, niv, fil, col, punt);
-				}
-				if (fil == 0 && col == 4) {
-					// CAMBIAR NIVEL
-					niveles(tab, niv);
-				}
-
-			} while ((fil > -1 && fil < 1) || (col > 0 && col < 5));
-
-			// CASILLA EN LA QUE GOLPEA
-			
-			if (tab[fil][col] == 0) {
-				tab[fil][col] = 3;
-				golp++;
-			} else {
-				if (tab[fil][col] == 1) {
-					tab[fil][col] = 0;
-				}
-				if (tab[fil][col] == 2) {
-					tab[fil][col] = 1;
-				}
-
-				if (tab[fil][col] == 3) {
-					tab[fil][col] = 2;
-				}
-			}
-
-			// CASILLA LADO DERECHO
-			if (tab[fil][col + 1] == 0) {
-				tab[fil][col + 1] = 3;
 				
-
-			} else {
-				if (tab[fil][col + 1] == 1) {
-					tab[fil][col + 1] = 0;
-				}
-				if (tab[fil][col + 1] == 2) {
-					tab[fil][col + 1] = 1;
-				}
-
-				if (tab[fil][col + 1] == 3) {
-					tab[fil][col + 1] = 2;
-				}
+			} while ((fil < -1 || fil > 6) || (col < -2 || col > 6) || col == -1);
+			if (fil == 0 && col == 1) {
+				// RECOMENZAR
+				flag = true;
+				golp=0;
+				tablero(niv, tab, flag);
+			}
+			if (fil == 0 && col == 2) {
+				// NUEVO
+				golp=0;
+				tablero(niv, tab, flag);
+			}
+			if (fil == 0 && col == 3) {
+				// CALIFICACION
+				golp--;
+				puntuacion(contjugadas, fil, col);
+			}
+			if (fil == 0 && col == 4) {
+				// CAMBIAR NIVEL
+				golp=0;
+				niveles(tab);
 			}
 
-			// CASILLA LADO IZQUIERDO
-			if (tab[fil][col - 1] == 0) {
-				tab[fil][col - 1] = 3;
 
-			} else {
-				if (tab[fil][col - 1] == 1) {
-					tab[fil][col - 1] = 0;
-				}
-				if (tab[fil][col - 1] == 2) {
-					tab[fil][col - 1] = 1;
-				}
+			if (fil != 0) {
 
-				if (tab[fil][col - 1] == 3) {
-					tab[fil][col - 1] = 2;
-				}
-			}
+				// CASILLA EN LA QUE GOLPEA
 
-			// CASILLA ARRIBA
+				if (tab[fil][col] == 0) {
+					tab[fil][col] = 3;
+				} else {
+					if (tab[fil][col] == 1) {
+						tab[fil][col] = 0;
+					}
+					if (tab[fil][col] == 2) {
+						tab[fil][col] = 1;
+					}
 
-			if (tab[fil - 1][col] == 0) {
-				tab[fil - 1][col] = 3;
-
-			} else {
-				if (tab[fil - 1][col] == 1) {
-					tab[fil - 1][col] = 0;
-				}
-				if (tab[fil - 1][col] == 2) {
-					tab[fil - 1][col] = 1;
+					if (tab[fil][col] == 3) {
+						tab[fil][col] = 2;
+					}
 				}
 
-				if (tab[fil - 1][col] == 3) {
-					tab[fil - 1][col] = 2;
-				}
-			}
+				// CASILLA LADO DERECHO
+				if (tab[fil][col + 1] == 0) {
+					tab[fil][col + 1] = 3;
 
-			// CASILLA ABAJO
-			if (tab[fil + 1][col] == 0) {
-				tab[fil + 1][col] = 3;
+				} else {
+					if (tab[fil][col + 1] == 1) {
+						tab[fil][col + 1] = 0;
+					}
+					if (tab[fil][col + 1] == 2) {
+						tab[fil][col + 1] = 1;
+					}
 
-			} else {
-				if (tab[fil + 1][col] == 1) {
-					tab[fil + 1][col] = 0;
-				}
-				if (tab[fil + 1][col] == 2) {
-					tab[fil + 1][col] = 1;
+					if (tab[fil][col + 1] == 3) {
+						tab[fil][col + 1] = 2;
+					}
 				}
 
-				if (tab[fil + 1][col] == 3) {
-					tab[fil + 1][col] = 2;
+				// CASILLA LADO IZQUIERDO
+				if (tab[fil][col - 1] == 0) {
+					tab[fil][col - 1] = 3;
+
+				} else {
+					if (tab[fil][col - 1] == 1) {
+						tab[fil][col - 1] = 0;
+					}
+					if (tab[fil][col - 1] == 2) {
+						tab[fil][col - 1] = 1;
+					}
+
+					if (tab[fil][col - 1] == 3) {
+						tab[fil][col - 1] = 2;
+					}
+				}
+
+				// CASILLA ARRIBA
+
+				if (tab[fil - 1][col] == 0) {
+					tab[fil - 1][col] = 3;
+
+				} else {
+					if (tab[fil - 1][col] == 1) {
+						tab[fil - 1][col] = 0;
+					}
+					if (tab[fil - 1][col] == 2) {
+						tab[fil - 1][col] = 1;
+					}
+
+					if (tab[fil - 1][col] == 3) {
+						tab[fil - 1][col] = 2;
+					}
+				}
+
+				// CASILLA ABAJO
+				if (tab[fil + 1][col] == 0) {
+					tab[fil + 1][col] = 3;
+
+				} else {
+					if (tab[fil + 1][col] == 1) {
+						tab[fil + 1][col] = 0;
+					}
+					if (tab[fil + 1][col] == 2) {
+						tab[fil + 1][col] = 1;
+					}
+
+					if (tab[fil + 1][col] == 3) {
+						tab[fil + 1][col] = 2;
+					}
 				}
 			}
 
 			// COMPROBAR SI GANO
 			int cont = 0;
-			int i = 0, j = 0;
-			for (i = 0; i < 6; i++) {
-
-				for (j = 0; j < 6; j++) {
+			int i = 1, j = 1;
+			for (i = 1; i < 7; i++) {
+				for (j = 1; j < 7; j++) {
 					if (tab[i][j] == 0) {
 						cont++;
 
 					}
 				}
 				if (cont == 36) {
+					mensaje(niv, golp);
 					cont = 0;
 					golp = 0;
-					puntuacion(contjugadas, golp, niv, fil, col, punt);
+
 				}
 
 			}
@@ -296,53 +348,59 @@ public class juego {
 		return golp;
 
 	}
-
-	public static double puntuacion(int contpartidas, int golpes, int fil, int col, int niv, double punt) {
-		int calificacion = 0;
+	public static double puntuacion(int contpartidas, int fil, int col) {
+		double calificacion = 0;
 		double aux = punt;
-
+		
 		// si es la primera vez que se juega
-
 		if (contpartidas == 1) {
 			calificacion = 1;
 		}
-
-		// calculo de la puntuación de los niveles
-		if (niv == 1) {
-			calificacion = 3 / golpes;
-		} else if (niv == 2) {
-			calificacion = 6 / golpes;
-		} else if (niv == 3) {
-			calificacion = 9 / golpes;
-		} else if (niv == 4) {
-			calificacion = 12 / golpes;
-		} else if (niv == 5) {
-			calificacion = 15 / golpes;
-		} else if (niv == 6) {
-			calificacion = 18 / golpes;
-		} else if (niv == 7) {
-			calificacion = 21 / golpes;
-		} else if (niv == 8) {
-			calificacion = 24 / golpes;
-		} else if (niv == 9) {
-			calificacion = 27 / golpes;
-		}
-
+		
 		// abandono juego
-		if (fil == 0 && col == -2 & golpes == 0) {
-			calificacion = 0;
+				if (fil == 0 && col == -2 && golp == 0) {
+					calificacion = 0.5;
+				}
+				if (fil == 0 && col == -2 && golp > 0) {
+					calificacion = 1;
+				}
+		int contadorjug=0;
+		while (golp > 0 && contadorjug == 0) {
+			// calculo de la puntuación de los niveles
+			// los golpes recomendados son el numero del nivel por 3
+			if (niv == 1) {
+				calificacion = 3 / golp;
+			} else if (niv == 2) {
+				calificacion = 6 / golp;
+			} else if (niv == 3) {
+				calificacion = 9 / golp;
+			} else if (niv == 4) {
+				calificacion = 12 / golp;
+			} else if (niv == 5) {
+				calificacion = 15 / golp;
+			} else if (niv == 6) {
+				calificacion = 18 / golp;
+			} else if (niv == 7) {
+				calificacion = 21 / golp;
+			} else if (niv == 8) {
+				calificacion = 24 / golp;
+			} else if (niv == 9) {
+				calificacion = 27 / golp;
+			}
+			contadorjug++;
 		}
-		if (fil == 0 && col == -2 & golpes > 0) {
-			calificacion = 1;
-		}
+		
+		punt = calificacion;
+		System.out.println(punt + " Puntuacion");
+		
 
-		punt += calificacion;
-
+		
+		
 		if (punt > aux) {
 			File archivo = new File("archivo.txt");
 			BufferedWriter bw = null;
 
-			System.out.println(aux);
+			
 			try {
 				if (archivo.exists()) {
 					// El fichero existe
@@ -383,5 +441,8 @@ public class juego {
 				System.out.println("Hecho en " + golp + " golpes");
 			}
 		}
+		int contpartidas=0;
+		contpartidas++;
+		puntuacion(contpartidas, aux, golp);
 	}
 }
